7537b6ca768300eac7091bc953adbd81d6c6deae
lwyang <1670906161@qq.com>
2019-11-11 10:55:21 +0800

    chore: init commit

---
+++ src/main/java/com/lwyang/Totoro/TotoroApplication.java
@@ -0,0 +1,13 @@
+package com.lwyang.Totoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lwyang/Totoro/TotoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwyang.Totoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
