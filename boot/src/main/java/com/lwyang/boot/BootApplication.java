a884ec16e5cb96c096b050268f1b0212f7cd1e42
lwyang <1670906161@qq.com>
2019-11-12 09:22:56 +0800

    feat[boot]: modified BootApplication.java to boostrap

--- boot/src/main/java/com/lwyang/boot/BootApplication.java
+++ boot/src/main/java/com/lwyang/boot/BootApplication.java
@@ -3,7 +3,7 @@ package com.lwyang.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.lwyang")
 public class BootApplication {
 
     public static void main(String[] args) {
