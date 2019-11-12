92e2d2d21ab49448803ebac782b48036ad91eea9
lwyang <1670906161@qq.com>
2019-11-12 19:52:54 +0800

    chore[customer]: modified _create_customer_table.sql

--- customer/src/main/resources/db/migration/V1__create_customer_table.sql
+++ customer/src/main/resources/db/migration/V1__create_customer_table.sql
@@ -1,18 +1,26 @@
-CREATE TABLE `customer` (
-  `id` bigint(64) NOT NULL COMMENT '主键',
-  `username` varchar(10) NOT NULL COMMENT '用户名',
-  `password` varchar(32) NOT NULL COMMENT '用户密码',
-  `avatar` varchar(128) DEFAULT NULL COMMENT '用户头像地址',
-  `email` varchar(32) DEFAULT NULL COMMENT '用户邮箱',
-  `age` int(4) DEFAULT NULL COMMENT '用户年龄',
-  `sex` tinyint(2) DEFAULT NULL COMMENT '用户性别 1:男 2:女',
-  `points` int(4) DEFAULT NULL COMMENT '用户积分',
-  `status` tinyint(2) NOT NULL COMMENT '用户状态 0：禁用 1：正常',
-  `login_time` datetime DEFAULT NULL COMMENT '上次登录时间',
-  `login_ip` varchar(32) DEFAULT NULL COMMENT '上次登录ip地址',
-  `create_time` datetime NOT NULL COMMENT '用户注册时间',
-  `modify_time` datetime NOT NULL COMMENT '用户修改时间',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+create table customer
+(
+	id bigint not null comment 'id',
+	username varchar(10) not null comment '用户名',
+	password varchar(32) not null comment '密码',
+	answer varchar(128) not null comment '密保答案',
+	question varchar(128) not null comment '找回密码问题',
+	avatar varchar(128) null comment '头像地址',
+	email varchar(32) not null comment '邮箱地址',
+	age int null comment '年龄',
+	sex tinyint default 1 null comment '1：男 2：女',
+	points int null comment '积分',
+	status tinyint default 1 not null comment '0：禁用 1：正常',
+	login_time datetime null comment '上次登录时间',
+	login_ip varchar(32) null comment '上次登录ip',
+	create_time datetime not null comment '注册时间',
+	modify_time datetime not null comment '用户信息修改时间',
+	constraint customer_pk primary key (id)
+)comment '顾客表' ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+create unique index customer_email_uindex
+	on customer (email);
+create unique index customer_username_uindex
+	on customer (username);
+
+
 
