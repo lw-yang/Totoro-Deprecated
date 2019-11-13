b4204b68ee66bcffed41df48d35e342b02463b5b
lwyang <1670906161@qq.com>
2019-11-13 16:27:42 +0800

    chore[common]: add common module

---
+++ common/src/test/java/com/lwyang/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwyang.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
