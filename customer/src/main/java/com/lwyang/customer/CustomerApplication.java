43a7bcf8298d60a12870335c4a10c7a90235618d
lwyang <1670906161@qq.com>
2019-11-13 14:16:26 +0800

    fix[customer]: fix customer build

--- customer/src/main/java/com/lwyang/customer/CustomerApplication.java
+++ customer/src/main/java/com/lwyang/customer/CustomerApplication.java
@@ -1,4 +1,13 @@
 package com.lwyang.customer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.lwyang.customer")
 public class CustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
+
 }
