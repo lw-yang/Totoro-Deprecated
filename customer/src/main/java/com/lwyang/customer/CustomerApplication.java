67051cd32608557a6da4b1ba572e826a96d0c3e2
lwyang <1670906161@qq.com>
2019-11-14 17:04:45 +0800

    feat[customer]: 增加@MapperScan扫描dao接口

--- customer/src/main/java/com/lwyang/customer/CustomerApplication.java
+++ customer/src/main/java/com/lwyang/customer/CustomerApplication.java
@@ -1,9 +1,11 @@
 package com.lwyang.customer;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.lwyang.customer")
+@MapperScan(basePackages = "com.lwyang.customer.dao")
 public class CustomerApplication {
 
     public static void main(String[] args) {
