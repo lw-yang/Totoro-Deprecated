2366ec02d52c11df63586b5a14a96aff5cd71aeb
lwyang <1670906161@qq.com>
2019-11-19 14:57:00 +0800

    feat[common]: 解决跨域问题

---
+++ common/src/main/java/com/lwyang/common/config/CorsConfiguration.java
@@ -0,0 +1,28 @@
+package com.lwyang.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域请求处理
+ * @author lwyang
+ */
+@Configuration(proxyBeanMethods = false)
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
